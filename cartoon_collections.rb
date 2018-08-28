c1aebcad64e90be408ca30878841f1f0884e97a6
Amalie S <amaliesmidth@gmail.com>
2018-08-28 13:58:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each do |dwarf|
+    puts dwarf
 end
 
 def summon_captain_planet# code an argument here
