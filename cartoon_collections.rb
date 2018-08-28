7650403fb83c7884751baeb12cb22cc02c620cfc
Amalie S <amaliesmidth@gmail.com>
2018-08-28 15:10:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-calls.any? do |call|
+  calls.any? do |call|
     call.length > 4
+  end
 end
 
 # words = ["are", "any", "of", "these", "words", "longer", "than", "four", "letters"]
