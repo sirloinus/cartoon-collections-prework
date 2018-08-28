c402445b114ae49ffb7f41a767a52bc1daf0fd3e
Amalie S <amaliesmidth@gmail.com>
2018-08-28 15:54:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,11 @@ def find_the_cheese(food)
   end
 end
 
-
+def find_the_cheese(food)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  food.find do |
+  end
+end
 
 # def find_the_cheese(food)
 #   cheese_types = ["cheddar", "gouda", "camembert"]
